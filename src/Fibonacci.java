554126305b4341a9a0bbb87654a56e42e84b7838
Simeon Kakpovi <skakpovi@gmail.com>
2016-09-08 12:52:12 -0400

    fibb  + collatz

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,22 +1,40 @@
 
 public class Fibonacci {
+	/*
+	 * Fibonacci and Collatz
+	 */
 	
 	public static void main (String[] args){
 		for(int i=1; i<20; i++){		
-			System.out.print(recurse(i) + " ");
+			System.out.print(fibonacci(i) + " \n");
 		}
+		
+		System.out.println(collatz(233444));
 				
 	}//end main
 	
-	public static int recurse(int x){
+	public static int fibonacci(int x){
 		if(x == 1){
 			return 1;
 		} else if(x== 2){
 			return 1;
 		} else {
-			int value = recurse(x-2) + recurse(x-1);
+			int value = fibonacci(x-2) + fibonacci(x-1);
 			return value;
 		}
-	}//end recurse
+	}//end fibonacci
+	
+	
+	public static int collatz(int x){
+		
+		if (x == 1 ){
+			return x;
+		} else if(x%2 == 0){
+			return collatz(x/2);
+		} else {
+			return collatz((x * 3)+1);
+		}
+		
+	}//end collatz
 	
 }//end class
