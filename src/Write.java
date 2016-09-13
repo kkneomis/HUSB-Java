025d478ce05f6404c94e0e35260798d7005e20b7
Simeon Kakpovi <skakpovi@gmail.com>
2016-09-13 18:13:48 -0400

    Book enhancedment

---
+++ src/Write.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Write{
+
+      public static void main(String[] args){
+    	 System.out.println("What would you like to print?");
+         Scanner sc = new Scanner(System.in);
+         String input = sc.nextLine();
+          System.out.println(input);
+
+     }
+
+}
